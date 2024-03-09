bfacb54dd7ee9f271f57a4588ca54a9d2f6dc54a
LJ S.A <lvjhn.d@gmail.com>
2024-03-09 15:05:12 +0800

    General Update (Restructuring)

--- backend/scans/data/MessageDownloader.py
+++
@@ -1,8 +0,0 @@
-
-class MessageDownloader: 
-    
-    def __init__(self, video_id): 
-        self.video_id = video_id 
-
-    def get_message(self): 
-        pass 
--- backend/static/mock-client.js
+++ backend/static/mock-client.js
@@ -4,23 +4,5 @@
 
 
 (() => {
-    const scansClient = 
-        new SCANSClient(
-            [
-                "VRBfUcbmYZA",
-                "8K7dHdcGhwo",
-                "AhM-PEO65Zw"
-            ], 
-            {
-                connectSelf : true
-            }
-        ); 
-
-    scansClient.onRetrievedVideoInfos = (infos) => {
-        /**
-         * Process retrieved video information.
-         */
-    }
-
-    window.scansClient = scansClient;
+  
 })() 
