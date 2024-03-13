1855491840f5c981f945ae06573c7317782efef3
LJ S.A <lvjhn.d@gmail.com>
2024-03-13 21:32:56 +0800

    Set up base structure (slight update)

--- backend/scans/api/api_server.py
+++ backend/scans/api/api_server.py
@@ -8,10 +8,9 @@ class APIServer:
     def __init__(self):
         Logger.clear("api_server")
         Logger.log("api_server", "> Hello, from API Server!")
-        pass
         
     def init(self):
         Logger.log("api_server", "> Initializing API Server...") 
 
     def run(self):
-        pass 
+        Logger.log("api_server", "> Running API Server...")
